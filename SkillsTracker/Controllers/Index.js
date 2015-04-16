c1f4f6e29e479d94fa6194b7fb016fd4b748ce18
Richard <r.avengraus@gmail.com>
2015-04-16 14:45:08 -0400

    delete files from master

--- SkillsTracker/Controllers/Index.js
+++
@@ -1,4 +0,0 @@
-﻿module.exports.person = require('./Person.js');
-module.exports.client = require('./Client.js');
-module.exports.project = require('./Project.js');
-module.exports.technology = require('./Technology.js');
